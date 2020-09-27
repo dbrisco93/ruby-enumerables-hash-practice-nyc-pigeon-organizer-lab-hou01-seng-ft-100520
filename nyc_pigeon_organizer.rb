1feb7504d4ec517f65b705c490632bf5c0905361
Dominique Brisco <dbrisco93@gmail.com>
2020-09-27 06:18:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |color_key, names|
       names.each do |name|
-        binding.pry
+        #binding.pry
       if !new_array[name]
         new_array[name] = {}
       end
