15602479a0ad60296e768632ca9c0747151093c7
Dominique Brisco <dbrisco93@gmail.com>
2020-09-27 05:58:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,13 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |color_key, names|
       names.each do |name|
       new_array << name
       end
     end
-    new_array
-    binding.pry
   end  
-  binding.pry
+
 end
